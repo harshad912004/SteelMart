a1fa4eccdfe5e7270998b278ae34b9e2fe2defc4
harshad912004 <harshaddhongade0901@gmail.com>
2026-06-14 14:01:13 +0530

    new file:   .gitignore 	modified:   Backend/.gitignore 	modified:   Backend/package.json 	modified:   Frontend/.gitignore 	modified:   Frontend/package-lock.json 	modified:   Frontend/package.json 	modified:   Frontend/vite.config.js 	new file:   package-lock.json 	modified:   package.json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,20 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+    proxy: {
+      '/employee': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/vendor': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     outDir: 'build',
